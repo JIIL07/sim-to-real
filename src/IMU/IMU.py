97e6bbd7bc9521c926c9619fc5095ff4d5e122f6
auwesome0au <trexrexrex12789@gmail.com>
2026-06-26 20:03:20 +0300

    нода паблишит данные в шину imu/raw_data

--- src/IMU/IMU.py
+++ src/IMU/IMU.py
@@ -1,62 +1,97 @@
-import time
-import csv
-from datetime import datetime
+import sys
+import rclpy
 from witmotion import IMU
+import math
+from rclpy.node import Node
+from sensor_msgs.msg import Imu
 
 PORT = '/dev/ttyUSB0'
 BAUD = 38400
-CSV_FILE = 'imu_data_log.csv'
 
+def converter(accel, gyro, angle):
+    #converting from g to m/s^2
+    accel_ms2 = {
+        'x': accel[0],
+        'y': accel[1],
+        'z': accel[2]
+    }
+    #converting degrees to radians
+    gyro_rads = {
+        'x': math.radians(gyro[0]),
+        'y': math.radians(gyro[1]),
+        'z': math.radians(gyro[2])
+    }
+    #converting degrees to quaternions
+    roll = math.radians(angle[0])
+    pitch = math.radians(angle[1])
+    yaw = math.radians(angle[2])
+
+    qx = math.sin(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) - math.cos(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+    qy = math.cos(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2) + math.sin(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2)
+    qz = math.cos(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2) - math.sin(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2)
+    qw = math.cos(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) + math.sin(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+
+    quaternion = {'x': qx, 'y': qy, 'z': qz, 'w': qw}
+
+    return {
+        'linear_acceleration': accel_ms2,
+        'angular_velocity': gyro_rads,
+        'orientation': quaternion
+    }
+
+class IMU_Node(Node):
+    def __init__(self, PORT, BAUD):
+        super().__init__("imu_node")
+        self.publisher = self.create_publisher(Imu, '/imu/data_raw', 10)
+
+        try:
+            self.imu = IMU(path=PORT, baudrate=BAUD)
+        except Exception:
+            sys.exit(1)
+
+        self.timer = self.create_timer(0.05, self.timer_callback)
+
+    def timer_callback(self):
+        accel = self.imu.get_acceleration()
+        gyro = self.imu.get_angular_velocity()
+        angle = self.imu.get_angle()
+
+        if accel is None or gyro is None or angle is None:
+            return
+
+        data = converter(accel, gyro, angle)
+        #converting data to datatype in ros2 IMU
+        msg = Imu()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'imu_link'
+
+        msg.linear_acceleration.x = data['linear_acceleration']['x']
+        msg.linear_acceleration.y = data['linear_acceleration']['y']
+        msg.linear_acceleration.z = data['linear_acceleration']['z']
+
+        msg.angular_velocity.x = data['angular_velocity']['x']
+        msg.angular_velocity.y = data['angular_velocity']['y']
+        msg.angular_velocity.z = data['angular_velocity']['z']
+
+        msg.orientation.x = data['orientation']['x']
+        msg.orientation.y = data['orientation']['y']
+        msg.orientation.z = data['orientation']['z']
+        msg.orientation.w = data['orientation']['w']
+
+        self.publisher.publish(msg)
 
 def main():
-    print(f"[*] Подключение к IMU на порту {PORT} (baud_rate = {BAUD})...")
+    rclpy.init()
+    node = IMU_Node(PORT, BAUD)
     try:
-        imu = IMU(path=PORT, baudrate=BAUD)
-    except Exception as e:
-        print(f"[!] Ошибка подключения: {e}")
-        return
-
-    with open(CSV_FILE, mode='w', newline='') as f:
-        writer = csv.writer(f)
-        # Пишем шапку CSV
-        writer.writerow(['timestamp', 'accel_x', 'accel_y', 'accel_z',
-                         'gyro_x', 'gyro_y', 'gyro_z',
-                         'roll', 'pitch', 'yaw'])
-
-        print("[*] Соединение установлено")
-        try:
-            while True:
-                #данные
-                accel = imu.get_acceleration()
-                gyro = imu.get_angular_velocity()
-                angle = imu.get_angle()
-
-                #Ждем данные
-                if accel is None or gyro is None or angle is None:
-                    time.sleep(0.01)
-                    continue
-
-                # Генерируем таймстемп
-                ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-                # Печатать в терминал
-                print(f"[{ts}] "
-                      f"Accel(g): {accel[0]:5.2f} {accel[1]:5.2f} {accel[2]:5.2f} | "
-                      f"Gyro(°/s): {gyro[0]:7.2f} {gyro[1]:7.2f} {gyro[2]:7.2f} | "
-                      f"Angle(°): {angle[0]:6.2f} {angle[1]:6.2f} {angle[2]:6.2f}")
-
-                # Пишем данные в CSV лог
-                writer.writerow([ts, *accel, *gyro, *angle])
-
-                # Частота опроса (0.05 сек = 20 Гц)
-                time.sleep(0.05)
-
-        except KeyboardInterrupt:
-            print("\n[*] Остановка записи")
-        finally:
-            imu.close()
-            print(f"[*] Лог успешно сохранен в файл: {CSV_FILE}")
-
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if hasattr(node, 'imu'):
+            node.imu.close()
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
